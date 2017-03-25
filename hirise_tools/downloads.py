3012a83956b0c903e53f419282f9d0b53d673687
K.-Michael Aye <kmichael.aye@gmail.com>
2017-03-25 00:15:47 -0600

    Little fixes

--- hirise_tools/downloads.py
+++ hirise_tools/downloads.py
@@ -1,17 +1,17 @@
 from pathlib import Path
-from .products import PRODUCT_ID, HiRISE_URL, RED_PRODUCT_ID
-from six.moves.urllib.request import urlretrieve
+
 from six.moves.urllib.error import HTTPError
+from six.moves.urllib.request import urlretrieve
+
+from .products import PRODUCT_ID, RED_PRODUCT_ID, HiRISE_URL
 
 
 def hirise_dropbox():
-    home = Path.home()
-    return home / 'Dropbox' / 'data' / 'hirise'
+    return Path.home() / 'Dropbox' / 'data' / 'hirise'
 
 
 def labels_root():
-    dropbox = hirise_dropbox()
-    return dropbox / 'labels'
+    return hirise_dropbox() / 'labels'
 
 
 def get_rdr_some_label(kind, obsid):
@@ -81,11 +81,12 @@ def get_rdr_color_label(obsid):
 
 
 def download_product(prodid_path, saveroot=None):
-    saveroot = Path(saveroot)
     if saveroot is None:
         saveroot = hirise_dropbox()
-    elif not saveroot.is_absolute():
-        saveroot = hirise_dropbox() / saveroot
+    else:
+        saveroot = Path(saveroot)
+        if not saveroot.is_absolute():
+            saveroot = hirise_dropbox() / saveroot
 
     saveroot.mkdir(exist_ok=True)
     url = HiRISE_URL(prodid_path)
@@ -105,16 +106,16 @@ def download_RED_product(obsid, ccdno, channel, saveroot=None, overwrite=False):
         saveroot = Path(saveroot)
         if not saveroot.is_absolute():
             saveroot = hirise_dropbox() / saveroot
-    saveroot.mkdir(exist_ok=True)
 
     pid = RED_PRODUCT_ID(obsid, ccdno, channel)
-    savepath = saveroot / pid.fname
+    savepath = saveroot / obsid / pid.fname
+    savepath.parent.mkdir(parents=True, exist_ok=True)
 
     # if file already is there:
     if savepath.exists() and not overwrite:
         return savepath
 
-    print("Downloading\n", pid.furl, 'to\n', savepath)
+    print("Downloading\n", pid.furl, '\nto\n', savepath)
     try:
         urlretrieve(pid.furl, str(savepath))
     except HTTPError as e:
