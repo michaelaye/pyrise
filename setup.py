48aea22a0b540d6c0af0c8f34b9d4db73259fbd9
K.-Michael Aye <kmichael.aye@gmail.com>
2017-06-20 15:56:20 -0600

    change python version to 3.6 and add requirements

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Utilities',
     ],
     test_suite='tests',
